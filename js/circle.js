81111d37aaadf3025f427647f9b950f9b9cb4159
septemberEatMe <vitaliiseptember@gmail.com>
2019-12-07 12:05:46 +0200

    refactor code

---
+++ js/circle.js
@@ -0,0 +1,12 @@
+function f1(){
+	var p;
+	p = document.getElementById('out');
+	// заданное количество повторений
+	//i++  i=i+1
+
+	for (var i=1; i<=100; i++){
+		p.innerHTML += i + ' ';
+	}
+
+
+}
